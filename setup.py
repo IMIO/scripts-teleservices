086855393c38fcc569c3be53a7b982752fde1b54
Michaël Colicchia <m.colicchia@imio.be>
2019-09-30 14:59:48 +0200

    [IMP] adapt setup.py

--- setup.py
+++ setup.py
@@ -9,25 +9,23 @@ import os
 class inst(install):
     def run(self):
         install.run(self)
-        path = os.getcwd().replace(" ", "\ ").replace("(","\(").replace(")","\)") + "/bin/"
-        os.system("sh "+path+"install_imio-ts-aes.sh")
 
 setup(
-    name='imio-ts-aes',
-    author='Christophe Boulanger',
-    author_email='christophe.boulanger@imio.be',
+    name='scripts-teleservices',
+    author='Nicolas Hislaire',
+    author_email='nicolas.hislaire@imio.be',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/IMIO/imio-ts-aes',
+    url='https://github.com/IMIO/scripts-teleservices',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Environment :: Web Environment',
+        'Environment :: Console',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
     ],
     zip_safe=False,
     cmdclass={

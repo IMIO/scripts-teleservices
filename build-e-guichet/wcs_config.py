7dfa68b8e964a56860fd760c47875b25903c8162
Daniel Muyshond <daniel.muyshond@gmail.com>
2020-04-23 17:05:25 +0200

    remove unused statement

--- build-e-guichet/wcs_config.py
+++ build-e-guichet/wcs_config.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # sudo -u  wcs wcsctl -f /etc/wcs/wcs-au-quotidien.cfg runscript --vhost=$1-formulaires.$2 /opt/publik/scripts/build-e-guichet/wcs_config.py $1
 
-import os
 import sys
 from quixote import get_publisher
 

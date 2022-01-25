fa89c29f93294b1c39aba9b5fa75a86d263eaf4e
Daniel Muyshond <daniel.muyshond@imio.be>
2022-01-25 10:21:37 +0100

    [MTELEEUPA-38] add working script to clean all forms in all baskets (2nd)

--- python-scripts/delete_from_basket.py
+++ python-scripts/delete_from_basket.py
@@ -1,5 +1,8 @@
 """
 Attention, supprime toutes les demandes de tous les paniers
+
+Usage : sudo -u combo combo-manage tenant_command runscript -d ville.guichet-citoyen.be clean_baskets.py
+
 """
 
 from combo.apps.lingo.models import BasketItem

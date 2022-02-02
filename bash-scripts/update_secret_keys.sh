f99e68e89ce5232b6b36c1c793343cf83287d1cf
Daniel Muyshond <daniel.muyshond@imio.be>
2022-02-02 16:38:26 +0100

    [INFRA-4237] fix passerelle paths

--- bash-scripts/update_secret_keys.sh
+++ bash-scripts/update_secret_keys.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Helps to resolve INFRA-4237 TELE-1236
 
-BRICKS=('/etc/chrono/secret' '/etc/bijoe/secret' '/etc/hobo/secret' '/etc/combo/secret' '/etc/fargo/secret' '/etc/authentic2-multitenant/secret' '/etc/wcs/secret')
+BRICKS=('/etc/chrono/secret' '/etc/bijoe/secret' '/etc/hobo/secret' '/etc/passerelle/secret' '/etc/combo/secret' '/etc/fargo/secret' '/etc/authentic2-multitenant/secret' '/etc/wcs/secret')
 
 for el in "${BRICKS[@]}"
 do

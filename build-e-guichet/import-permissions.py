a9f0b94e0b46387e8b70182a778d40892146c11d
Daniel Muyshond <daniel.muyshond@imio.be>
2022-04-21 14:18:56 +0200

    [TELE-547] add cards access permissions for 'agent ayant accees aux fabr.'

--- build-e-guichet/import-permissions.py
+++ build-e-guichet/import-permissions.py
@@ -1,5 +1,6 @@
-from quixote import get_publisher
 import sys
+
+from quixote import get_publisher
 
 pub = get_publisher()
 permissions = {
@@ -9,13 +10,15 @@ permissions = {
     'bounces': [],
     'forms': [],
     'workflows': [],
-    'categories': []
+    'categories': [],
+    'cards': []
 }
 
 with open("/tmp/tmp_uuid_agent_fabriques.txt", 'r') as file_aa:
     uuid_aa = file_aa.read()
     permissions['forms'].append(uuid_aa)
     permissions['categories'].append(uuid_aa)
+    permissions['cards'].append(uuid_aa)
     if len(sys.argv) > 1 and sys.argv[1] == 'full':
         permissions['workflows'].append(uuid_aa)
     file_aa.close()
@@ -27,6 +30,7 @@ with open("/tmp/tmp_uuid_debug.txt", 'r') as file_debug:
     permissions['workflows'].append(uuid_debug)
     permissions['settings'].append(uuid_debug)
     permissions['bounces'].append(uuid_debug)
+    permissions['cards'].append(uuid_debug)
 
 pub.cfg['admin-permissions'] = permissions
 pub.write_cfg()

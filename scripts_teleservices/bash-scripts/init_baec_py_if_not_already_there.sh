29b2e24889a15efaffde83befbf7cf6c52be27e6
Nicolas Selva <nicolas.selva@imio.be>
2023-05-11 11:15:29 +0200

    [TELE-1682]fix pwd

--- scripts_teleservices/bash-scripts/init_baec_py_if_not_already_there.sh
+++ scripts_teleservices/bash-scripts/init_baec_py_if_not_already_there.sh
@@ -2,6 +2,8 @@
 
 set -e # Exit immediately if a command exits with a non-zero status.
 
+CURRENT_DIR=/opt/publik/scripts/scripts_teleservices/bash-scripts
+
 # setup passerelle connector
 BAEC=/etc/passerelle/settings.d/baec.py
 BAEC_FILE=baec.py
@@ -10,7 +12,7 @@ if test -f "$BAEC"; then
     echo "$BAEC already exists. This is what we want so that's fine !"
 else
     echo "$BAEC does not exist yet so I'm gonna create it and reboot passerelle..."
-    cp $(pwd)/$BAEC_FILE /etc/passerelle/settings.d/ && service passerelle restart
+    cp CURRENT_DIR/$BAEC_FILE /etc/passerelle/settings.d/ && service passerelle restart
 fi
 
 CASIER=/etc/passerelle/settings.d/casier_judiciaire.py
@@ -19,5 +21,6 @@ if test -f "$CASIER"; then
     echo "$CASIER already exists. This is what we want so that's fine !"
 else
     echo "$CASIER does not exist yet so I'm gonna create it and reboot passerelle..."
-    cp $(pwd)/$CASIER_FILE /etc/passerelle/settings.d/ && service passerelle restart
+    echo $CURRENT_DIR/$CASIER_FILE
+    cp $CURRENT_DIR/$CASIER_FILE /etc/passerelle/settings.d/ && service passerelle restart
 fi

6757812aa99c9c6234b284ef4a885874e6a255bd
Daniel Muyshond <daniel.muyshond@imio.be>
2023-06-09 10:40:50 +0200

    [TELE-1736] Fix setup errors, Add proper feedback

--- scripts_teleservices/build-e-guichet/passerelle/build-api-user.py
+++ scripts_teleservices/build-e-guichet/passerelle/build-api-user.py
@@ -1,10 +1,16 @@
 from passerelle.base.models import ApiUser
 
-api_user = ApiUser.objects.create(
-    username='Tout_le_monde',
-    fullname='Tout le monde',
-    description='Tout le monde',
-    keytype='',
-    key=''
+api_user, created = ApiUser.objects.get_or_create(
+    username="Tout_le_monde",
+    defaults={
+        "fullname": "Tout le monde",
+        "description": "Tout le monde",
+        "keytype": "",
+        "key": "",
+    },
 )
 
+if created:
+    print(f"api_user '{api_user.username}' has been created.")
+else:
+    print(f"api_user '{api_user.username}' already exists. Fine.")

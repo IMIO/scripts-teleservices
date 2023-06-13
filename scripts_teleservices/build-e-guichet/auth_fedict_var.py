e296f6fa4d6b7f78a3eb5c422b67f41e5c8d8927
Daniel Muyshond <daniel.muyshond@imio.be>
2023-06-13 10:55:03 +0200

    [TELE-1736] Add comment for reference

--- scripts_teleservices/build-e-guichet/auth_fedict_var.py
+++ scripts_teleservices/build-e-guichet/auth_fedict_var.py
@@ -1,3 +1,11 @@
-# authentic2-multitenant-manage tenant_command runscript /opt/publik/scripts/scripts_teleservices/build-e-guichet/auth_var.py -d $1-auth.$2
+# authentic2-multitenant-manage tenant_command runscript /opt/publik/scripts/scripts_teleservices/build-e-guichet/auth_fedict_var.py -d $1-auth.$2
+# For reference or as documentation, here is the initial commit message related to this whole script : "Adapt script to have list of countries instead of plain text"
 from authentic2.models import Attribute
-Attribute.objects.filter(name='country').update(kind='country')
+
+try:
+    Attribute.objects.filter(name="country").update(kind="country")
+except Exception as e:
+    # log the exception
+    print(
+        f"❌ An error occurred while running  authentic2-multitenant-manage tenant_command runscript /opt/publik/scripts/scripts_teleservices/build-e-guichet/auth_fedict_var.py : {e}"
+    )

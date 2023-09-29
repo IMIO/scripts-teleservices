e8651237efbb0577d435206269bd4f8b9dbbfde7
Daniel Muyshond <daniel.muyshond@imio.be>
2023-09-29 12:02:06 +0200

    [TELE-1800] Add missing split

--- scripts_teleservices/python-scripts/check_authenticators.py
+++ scripts_teleservices/python-scripts/check_authenticators.py
@@ -21,7 +21,7 @@ tenant = subprocess.run(
 )
 
 if tenant:
-    tenant = tenant.stdout.decode("utf-8").strip()
+    tenant = tenant.stdout.decode("utf-8").split("\n")[0].strip()
     outputs = tenant.split("\n")  # if there is multiple elements in the tenants folder
     if len(outputs) > 1:
         for element in outputs:

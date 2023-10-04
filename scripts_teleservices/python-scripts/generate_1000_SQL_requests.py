4b2e8cd35dd6bc555b872d680a2d9941f2b4444a
Daniel Muyshond <daniel.muyshond@imio.be>
2023-10-04 11:14:39 +0200

    Add usage in docstringé

--- scripts_teleservices/python-scripts/generate_1000_SQL_requests.py
+++ scripts_teleservices/python-scripts/generate_1000_SQL_requests.py
@@ -2,15 +2,27 @@
 20230411 · See:
 https://dev.entrouvert.org/issues/81462#note-11
 
-Le script générera une requête SQL à chaque itération de la boucle, soit 1000 requêtes en total.
+Le script générera une requête SQL à chaque itération de la boucle, soit 1000 requêtes
+en total.
 
-User.objects.all()[:1] génère une requête SQL qui cherche le premier utilisateur dans la base de données.
+User.objects.all()[:1] génère une requête SQL qui cherche le premier utilisateur dans
+la base de données.
 
-Le script mesure le temps écoulé entre chaque itération, ce qui peut aider à identifier des problèmes de performance.
+Le script mesure le temps écoulé entre chaque itération, ce qui peut aider à identifier
+des problèmes de performance.
 
-Si le temps d'exécution augmente de manière significative pendant les périodes de trafic élevé, cela peut indiquer un problème au niveau de la base de données PostgreSQL.
+Si le temps d'exécution augmente de manière significative pendant les périodes de trafic
+élevé, cela peut indiquer un problème au niveau de la base de données PostgreSQL.
 
 Par contre, si le temps d'exécution reste constant, le problème pourrait être ailleurs.
+
+Usage:
+
+sudo -u authentic-multitenant authentic2-multitenant-manage tenant_command \
+    runscript -d \
+    mettet-auth.guichet-citoyen.be \
+    /opt/publik/scripts/scripts_teleservices/python-scripts/generate_1000_SQL_requests.py
+
 """
 
 from django.contrib.auth import get_user_model

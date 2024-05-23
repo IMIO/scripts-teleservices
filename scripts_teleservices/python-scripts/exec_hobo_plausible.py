45418b415d8fd56eb265d7d9a421b0cc95a09ab5
jmodesto <jordano.modesto@imio.be>
2024-05-23 15:22:10 +0200

    [TELE-1920]Make it work - Hobo variable PLausible

---
+++ scripts_teleservices/python-scripts/exec_hobo_plausible.py
@@ -0,0 +1,42 @@
+import json
+import subprocess
+import os
+
+recipe_json_path = "/etc/hobo/recipe.json"
+hobo_json_path = "./hobo_variable.json"
+# Lire le fichier JSON recipe.json
+try:
+    with open(recipe_json_path, "r", encoding="utf-8") as file:
+        recipe_data = json.load(file)
+except FileNotFoundError:
+    print(f"Le fichier {recipe_json_path} n'a pas été trouvé.")
+    exit(1)
+except json.JSONDecodeError:
+    print(f"Erreur de décodage du fichier JSON {recipe_json_path}.")
+    exit(1)
+
+
+# Construction de la commande shell avec la valeur de "hobo" et execution avec subprocess
+# Extraire la valeur de "hobo"
+try:
+    hobo_tenant = recipe_data["variables"]["hobo"]
+except KeyError:
+    print("La clé 'hobo' n'a pas été trouvée dans le fichier JSON.")
+    exit(1)
+try:
+    subprocess.run(
+        [
+            "sudo",
+            "-u",
+            "hobo",
+            "hobo-manage",
+            "tenant_command",
+            "runscript",
+            "-d",
+            hobo_tenant,
+            "hobo_plausible_variable.py",
+        ]
+    )
+except subprocess.CalledProcessError as e:
+    print(f"Erreur lors de l'exécution de la commande shell : {e}")
+    exit(1)
--- scripts_teleservices/python-scripts/hobo_plausible_variable.py
+++ scripts_teleservices/python-scripts/hobo_plausible_variable.py
@@ -1,28 +1,12 @@
 import json
+import subprocess
 import os
 from hobo.environment.models import Variable
 
 # Chemin des fichiers JSON
+error_message = ""
 recipe_json_path = "/etc/hobo/recipe.json"
 hobo_json_path = "./hobo_variable.json"
-
-# Lire le fichier JSON recipe.json
-try:
-    with open(recipe_json_path, "r", encoding="utf-8") as file:
-        recipe_data = json.load(file)
-except FileNotFoundError:
-    print(f"Le fichier {recipe_json_path} n'a pas été trouvé.")
-    exit(1)
-except json.JSONDecodeError:
-    print(f"Erreur de décodage du fichier JSON {recipe_json_path}.")
-    exit(1)
-
-# Extraire la valeur de "combo"
-try:
-    combo_value = recipe_data["variables"]["combo"]
-except KeyError:
-    print("La clé 'combo' n'a pas été trouvée dans le fichier JSON.")
-    exit(1)
 
 
 # Lire le fichier JSON hobo_variable.json
@@ -31,13 +15,24 @@ def load_json_config():
         with open(hobo_json_path, "r", encoding="utf-8") as json_file:
             return json.load(json_file)
     except FileNotFoundError:
-        print(f"Le fichier {hobo_json_path} n'a pas été trouvé.")
-        exit(1)
+        error_message += f"Le fichier {hobo_json_path} n'a pas été trouvé."
     except json.JSONDecodeError:
         print(f"Erreur de décodage du fichier JSON {hobo_json_path}.")
-        exit(1)
 
 
+# Lire le fichier JSON recipe.json
+try:
+    with open(recipe_json_path, "r", encoding="utf-8") as file:
+        recipe_data = json.load(file)
+except FileNotFoundError:
+    error_message += f"Le fichier {recipe_json_path} n'a pas été trouvé."
+except json.JSONDecodeError:
+    error_message += f"Erreur de décodage du fichier JSON {recipe_json_path}."
+# Extraire la valeur de "combo"
+try:
+    combo_value = recipe_data["variables"]["combo"]
+except KeyError:
+    error_message += "La clé 'combo' n'a pas été trouvée dans le fichier JSON."
 hobo_variables = load_json_config()
 
 # Remplacer {{ commune_slug }} par combo_value dans les variables hobo
@@ -57,15 +52,11 @@ def create_or_update_variable(name, label, value):
     """
     variable, created = Variable.objects.update_or_create(name=name, defaults={"label": label, "value": value})
     if created:
-        print(f"Variable '{variable}' has been created.")
+        print(f"Variable '{variable}' has been created on {combo_value}, {error_message}")
     else:
-        print(f"Variable '{variable}' has been updated.")
+        print(f"Variable '{variable}' has been updated on {combo_value}, {error_message}")
 
 
 # Créer ou mettre à jour les variables avec les nouvelles valeurs
 for name, value in hobo_variables.items():
     create_or_update_variable(name, value["label"], value["value"])
-    print(f"Variable '{name}' has been created or updated.")
-    print(f"Label: '{value['label']}'")
-    print(f"Value: '{value['value']}'")
-    print("\n")
